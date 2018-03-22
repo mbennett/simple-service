6c5c7d2aa9353092929a12bade88f10a501e6a57
Matt Bennett <matt.bennett@vml.com>
2018-03-22 14:55:59 -0500

    I think it is working

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${testMessage}")
+    private String testMessage;
+
     @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "Hello! The testMessage is: " + this.testMessage;
     }
 
 }
